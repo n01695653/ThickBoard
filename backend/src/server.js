cb93457c14027a3b978f7a9231141c4aad192385
n01695653 <maksudmalek592@gmail.com>
2025-11-21 18:43:15 -0500

    ccompleted the Header Component and started notebox and improvement in backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,13 +2,16 @@ import express from  "express";
 import noteRoutes from "./routes/notesRoutes.js"
 import { connectdb } from "./config/db.js";
 import dotenv from "dotenv";
-
+import cors from "cors";
 dotenv.config();
 
 const app = express();
 const PORT  = process.env.PORT || 5001;
 connectdb();
 
+app.use(cors({
+    origin: "http://localhost:5173",
+}))
 //middleware = a function which run between request and response 
 app.use(express.json()) // this will parse(allow) json bodies : req.body
 
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
+import axios from "axios"
+import toast from "react-hot-toast";
 
 export const HomePage = () => {
+  const [notes,setNotes] = useState([]);
+  const [loading,setLoading] = useState(true);
+
+useEffect(() => {
+  const fetchNotes = async () => {
+    try {
+      const res = await axios.get("http://localhost:5001/api/notes")
+      console.log(res.data);
+      // Updating the notes state
+      setNotes(res.data.notes)
+    } catch (error) {
+      console.error("Error Fetching notes", error);
+      toast.error("Failed to Load notes");
+    } finally {
+      setLoading(false)
+    };
+  };
+  fetchNotes();
+  },[]);
   return (
     <div className='min-h-screen'>
       <Navbar/>
+      <div className='max-w-7xl mx-auto p-4 mt-6'>
+        {loading && <div className='text-center text-primary py-10'>Loading notes...</div>}
+        {notes && notes.length > 0 && (
+          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+            {notes.map(note => (
+              <div>
+                {note.title} | {note.content}
+              </div>
+            ))}
+
+          </div>
+        )}
+
+      </div>
     </div>
   )
 }
